8b1d2f14c43862e056566b9c2cb1dd49954df324
Mark Birbeck <mark.birbeck@gmail.com>
2013-07-08 19:08:24 +0100

    Add base engine with no methods.

---
+++ test/request-base.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var should = require('should');
+
+var _ = require('underscore');
+
+/**
+ * Add the BaseEngine engine to resourceful:
+ */
+
+var resourceful = require('resourceful');
+
+require('..').init(resourceful);
+
+describe('No Methods', function(){
+  var BaseEngine;
+
+  before(function(done){
+
+    /**
+     * Create a class that's connected to the BaseEngine:
+     */
+
+    BaseEngine = resourceful.define('dummy', function (){
+      this.use('BaseEngine', { 'uri': 'dummy' });
+      done();
+    });
+  });
+
+  it('should be no del method', function(done){
+    var id = '06675651';
+
+    BaseEngine.destroy(id, function(err, company){
+      should.exist(err);
+      err.should.have.property('message', 'No delete method.');
+      should.not.exist(company);
+      done();
+    });
+  });
+
+  it('should be no get method', function(done){
+    var id = '06675651';
+
+    BaseEngine.get(id, function(err, company){
+      should.exist(err);
+      err.should.have.property('message', 'No get method.');
+      should.not.exist(company);
+      done();
+    });
+  });
+
+  it('should be no put method', function(done){
+    BaseEngine.create({}, function(err, company){
+      should.exist(err);
+      err.should.have.property('message', 'No put method.');
+      should.not.exist(company);
+      done();
+    });
+  });
+});
